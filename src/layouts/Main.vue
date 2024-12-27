2b2214ec9ec2a6932636554c6741d82fb5f05cd2
Minnerva <potchara40@hotmail.com>
2024-12-27 08:34:34 +0700

    :bug: Fix non-active user can use

--- src/layouts/Main.vue
+++ src/layouts/Main.vue
@@ -1,25 +1,9 @@
 <template>
   <div class="w-full py-3 md:px-8 sm:px-6 px-4">
-    <!-- <template v-if="user">
-      <div v-if="!user.ign">
-        <Card>
-          <template v-slot:body>
-            <div class="grid grid-cols-2 gap-4">
-              <label class="col-span-full md:col-span-1">
-                Input your IGN or Discord Name: 
-                <InputBase v-model="ign"></InputBase>
-              </label>
-
-              <ButtonBase class="col-span-full md:col-start-1 md:col-end-2" :on-click="onSaveIgn" primary>Submit</ButtonBase>
-            </div>
-          </template>
-        </Card>
-      </div>
-
-      <div v-else-if="!user.active">Please contact Minnerva to activate your account.</div>
+    <template v-if="user !== false">
+      <div v-if="!user.active">Please contact Minnerva to activate your account.</div>
       <router-view v-else></router-view>
-    </template> -->
-    <router-view v-if="user !== false"></router-view>
+    </template>
   </div>
 </template>
 
